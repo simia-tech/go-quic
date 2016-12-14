df3009fd0f7a33198e61187dbbd519072d93567a
Philipp Brüll <bruell@simiatech.com>
2016-12-14 10:35:27 +0100

    more work on packet header

--- packet/header.go
+++ packet/header.go
@@ -7,16 +7,29 @@ import (
 
 // Header flags
 const (
-	PublicVersionFlag = 0x01
+	VersionFlag     = 0x01
+	PublicResetFlag = 0x02
 
-	PublicConnectionIDSize64 = 0x0c
+	ConnectionIDMask = 0x0c
+	ConnectionIDLen8 = 0x0c
+	ConnectionIDLen4 = 0x08
+	ConnectionIDLen1 = 0x04
+	ConnectionIDLen0 = 0x00
+
+	PacketNumberMask = 0x30
+	PacketNumberLen6 = 0x30
+	PacketNumberLen4 = 0x20
+	PacketNumberLen2 = 0x10
+	PacketNumberLen1 = 0x00
+
+	MaxHeaderSize = 19
 )
 
 // Header defines the packet header type.
 type Header []byte
 
-// SetPublic sets public header flags.
-func (h Header) SetPublic(flag uint8) {
+// SetFlag sets public header flags.
+func (h Header) SetFlag(flag uint8) {
 	if len(h) < 1 {
 		panic("buffer too small")
 	}
@@ -28,14 +41,55 @@ func (h Header) SetPublic(flag uint8) {
 func (h Header) SetConnectionID(value interface{}) {
 	switch v := value.(type) {
 	case uint64:
-		h.SetPublic(PublicConnectionIDSize64)
+		h.SetFlag(ConnectionIDLen8)
 		binary.LittleEndian.PutUint64(h[1:], v)
 	default:
 		panic(fmt.Sprintf("cannot set connection id value of type %T", v))
 	}
 }
 
+// SetPacketNumber sets the packet number and the corresponding header flags. The value has to be
+// uint8, uint16, uint32 or uint64. Values of other types will cause a panic.
+func (h Header) SetPacketNumber(value interface{}) {
+	offset := 1 + h.connectionIDLen()
+	switch v := value.(type) {
+	case uint64:
+		h.SetFlag(PacketNumberLen6)
+		binary.LittleEndian.PutUint32(h[offset:], uint32(v&0xffffffff))
+		binary.LittleEndian.PutUint16(h[offset+4:], uint16((v&0xffff00000000)>>32))
+	default:
+		panic(fmt.Sprintf("cannot set connection id value of type %T", v))
+	}
+}
+
 // Len returns the length of the header.
-func (h Header) Len() int {
+func (h Header) Len(special bool) int {
+	return 1 + h.connectionIDLen() + h.packetNumberLen(special)
+}
+
+func (h Header) connectionIDLen() int {
+	switch h[0] & ConnectionIDMask {
+	case ConnectionIDLen8:
+		return 8
+	case ConnectionIDLen4:
+		return 4
+	case ConnectionIDLen1:
+		return 1
+	}
 	return 0
+}
+
+func (h Header) packetNumberLen(special bool) int {
+	switch h[0] & PacketNumberMask {
+	case PacketNumberLen6:
+		return 6
+	case PacketNumberLen4:
+		return 4
+	case PacketNumberLen2:
+		return 2
+	}
+	if special {
+		return 0
+	}
+	return 1
 }
--- packet/header_test.go
+++ packet/header_test.go
@@ -7,28 +7,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHeader(t *testing.T) {
+func TestHeaderWrite(t *testing.T) {
 	testCases := []struct {
 		name          string
+		special       bool
 		flags         uint8
 		connectionID  interface{}
-		expectedLen   int
+		packetNumber  interface{}
 		expectedBytes []byte
 	}{
-		{"Version", packet.PublicVersionFlag, uint64(1),
-			0, []byte{0x0d, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+		{"Version", true, packet.VersionFlag, uint64(1), nil,
+			[]byte{0x0d, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+		{"PublicReset", true, packet.PublicResetFlag, uint64(1), nil,
+			[]byte{0x0e, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+		{"Regular", false, 0, uint64(1), uint64(2),
+			[]byte{0x3c, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00}},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			buffer := [19]byte{}
+			buffer := [packet.MaxHeaderSize]byte{}
 
 			header := packet.Header(buffer[:])
-			header.SetPublic(testCase.flags)
+			header.SetFlag(testCase.flags)
 			header.SetConnectionID(testCase.connectionID)
+			if testCase.packetNumber != nil {
+				header.SetPacketNumber(testCase.packetNumber)
+			}
+			l := header.Len(testCase.special)
 
-			assert.Equal(t, testCase.expectedLen, header.Len())
-			assert.Equal(t, testCase.expectedBytes, buffer[:])
+			assert.Equal(t, len(testCase.expectedBytes), l)
+			assert.Equal(t, testCase.expectedBytes, buffer[:l])
 		})
 	}
 }
