860f97129acacc90f1408d0fecbdc6b9e2eefa27
Philipp Brüll <bruell@simiatech.com>
2016-12-14 14:06:05 +0100

    more work on packet header

--- packet/header.go
+++ packet/header.go
@@ -7,6 +7,7 @@ import (
 
 // Header flags
 const (
+	FlagMask        = 0x03
 	VersionFlag     = 0x01
 	PublicResetFlag = 0x02
 
@@ -28,12 +29,20 @@ const (
 // Header defines the packet header type.
 type Header []byte
 
-// SetFlag sets public header flags.
-func (h Header) SetFlag(flag uint8) {
+// SetFlags sets public header flags.
+func (h Header) SetFlags(flag uint8) {
 	if len(h) < 1 {
 		panic("buffer too small")
 	}
 	h[0] |= flag
+}
+
+// Flags returns public header flags.
+func (h Header) Flags() uint8 {
+	if len(h) < 1 {
+		panic("buffer too small")
+	}
+	return h[0]
 }
 
 // SetConnectionID sets the connection id and the corresponding header flags. The value has to be
@@ -41,30 +50,127 @@ func (h Header) SetFlag(flag uint8) {
 func (h Header) SetConnectionID(value interface{}) {
 	switch v := value.(type) {
 	case uint64:
-		h.SetFlag(ConnectionIDLen8)
+		h.SetFlags(ConnectionIDLen8)
 		binary.LittleEndian.PutUint64(h[1:], v)
+	case uint32:
+		h.SetFlags(ConnectionIDLen4)
+		binary.LittleEndian.PutUint32(h[1:], v)
+	case uint8:
+		h.SetFlags(ConnectionIDLen1)
+		h[1] = v
 	default:
 		panic(fmt.Sprintf("cannot set connection id value of type %T", v))
 	}
+}
+
+// ConnectionID returns the connection id.
+func (h Header) ConnectionID() interface{} {
+	switch h[0] & ConnectionIDMask {
+	case ConnectionIDLen8:
+		if len(h) < 9 {
+			panic("buffer too small")
+		}
+		return binary.LittleEndian.Uint64(h[1:])
+	case ConnectionIDLen4:
+		if len(h) < 5 {
+			panic("buffer too small")
+		}
+		return binary.LittleEndian.Uint32(h[1:])
+	case ConnectionIDLen1:
+		if len(h) < 2 {
+			panic("buffer too small")
+		}
+		return h[1]
+	}
+	return nil
+}
+
+// SetVersion set the quic versions and the corresponding flag.
+func (h Header) SetVersion(version uint32) {
+	offset := 1 + h.connectionIDLen()
+	if len(h) < offset+4 {
+		panic("buffer too small")
+	}
+	h.SetFlags(VersionFlag)
+	binary.LittleEndian.PutUint32(h[offset:], version)
+}
+
+// Versions returns the versions.
+func (h Header) Version() uint32 {
+	offset := 1 + h.connectionIDLen()
+	if len(h) < offset+4 {
+		panic("buffer too small")
+	}
+	return binary.LittleEndian.Uint32(h[offset:])
 }
 
 // SetPacketNumber sets the packet number and the corresponding header flags. The value has to be
 // uint8, uint16, uint32 or uint64. Values of other types will cause a panic.
 func (h Header) SetPacketNumber(value interface{}) {
-	offset := 1 + h.connectionIDLen()
+	offset := 1 + h.connectionIDLen() + h.versionsLen()
 	switch v := value.(type) {
 	case uint64:
-		h.SetFlag(PacketNumberLen6)
+		if len(h) < offset+6 {
+			panic("buffer too small")
+		}
+		h.SetFlags(PacketNumberLen6)
 		binary.LittleEndian.PutUint32(h[offset:], uint32(v&0xffffffff))
 		binary.LittleEndian.PutUint16(h[offset+4:], uint16((v&0xffff00000000)>>32))
+	case uint32:
+		if len(h) < offset+4 {
+			panic("buffer too small")
+		}
+		h.SetFlags(PacketNumberLen4)
+		binary.LittleEndian.PutUint32(h[offset:], v)
+	case uint16:
+		if len(h) < offset+2 {
+			panic("buffer too small")
+		}
+		h.SetFlags(PacketNumberLen2)
+		binary.LittleEndian.PutUint16(h[offset:], v)
+	case uint8:
+		if len(h) < offset+1 {
+			panic("buffer too small")
+		}
+		h.SetFlags(PacketNumberLen1)
+		h[offset] = v
 	default:
-		panic(fmt.Sprintf("cannot set connection id value of type %T", v))
+		panic(fmt.Sprintf("cannot set packet number value of type %T", v))
 	}
+}
+
+// PacketNumber returns the connection id.
+func (h Header) PacketNumber() interface{} {
+	offset := 1 + h.connectionIDLen() + h.versionsLen()
+	switch h[0] & PacketNumberMask {
+	case PacketNumberLen6:
+		if len(h) < offset+6 {
+			panic("buffer too small")
+		}
+		return uint64(binary.LittleEndian.Uint32(h[offset:])) |
+			(uint64(binary.LittleEndian.Uint16(h[offset+4:])) << 32)
+	case PacketNumberLen4:
+		if len(h) < offset+4 {
+			panic("buffer too small")
+		}
+		return binary.LittleEndian.Uint32(h[offset:])
+	case PacketNumberLen2:
+		if len(h) < offset+2 {
+			panic("buffer too small")
+		}
+		return binary.LittleEndian.Uint16(h[offset:])
+	case PacketNumberLen1:
+		if len(h) < offset+1 {
+			return nil
+		}
+		return h[offset]
+	}
+	return nil
 }
 
 // Len returns the length of the header.
 func (h Header) Len(special bool) int {
-	return 1 + h.connectionIDLen() + h.packetNumberLen(special)
+	return 1 + h.connectionIDLen() + h.versionsLen() + h.packetNumberLen(special)
 }
 
 func (h Header) connectionIDLen() int {
@@ -75,6 +181,13 @@ func (h Header) connectionIDLen() int {
 		return 4
 	case ConnectionIDLen1:
 		return 1
+	}
+	return 0
+}
+
+func (h Header) versionsLen() int {
+	if h[0]&VersionFlag != 0 {
+		return 4
 	}
 	return 0
 }
--- packet/header_test.go
+++ packet/header_test.go
@@ -7,37 +7,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHeaderWrite(t *testing.T) {
+func TestHeader(t *testing.T) {
 	testCases := []struct {
-		name          string
-		special       bool
-		flags         uint8
-		connectionID  interface{}
-		packetNumber  interface{}
-		expectedBytes []byte
+		name         string
+		special      bool
+		flags        uint8
+		connectionID interface{}
+		version      interface{}
+		packetNumber interface{}
+
+		bytes []byte
 	}{
-		{"Version", true, packet.VersionFlag, uint64(1), nil,
-			[]byte{0x0d, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
-		{"PublicReset", true, packet.PublicResetFlag, uint64(1), nil,
+		{"VersionClient", false, 0, uint64(1), uint32(2), uint64(3),
+			[]byte{0x3d, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00}},
+		{"VersionServer", true, 0, uint64(1), nil, nil,
+			[]byte{0x0c, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+		{"PublicReset", true, packet.PublicResetFlag, uint64(1), nil, nil,
 			[]byte{0x0e, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
-		{"Regular", false, 0, uint64(1), uint64(2),
+		{"Regular8", false, 0, uint64(1), nil, uint64(2),
 			[]byte{0x3c, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00}},
+		{"Regular4", false, 0, uint32(1), nil, uint32(2),
+			[]byte{0x28, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00}},
+		{"Regular2", false, 0, uint32(1), nil, uint16(2),
+			[]byte{0x18, 0x01, 0x00, 0x00, 0x00, 0x02, 0x00}},
+		{"Regular1", false, 0, uint8(1), nil, uint8(2),
+			[]byte{0x04, 0x01, 0x02}},
 	}
 
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			buffer := [packet.MaxHeaderSize]byte{}
+	t.Run("Write", func(t *testing.T) {
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				buffer := [packet.MaxHeaderSize]byte{}
 
-			header := packet.Header(buffer[:])
-			header.SetFlag(testCase.flags)
-			header.SetConnectionID(testCase.connectionID)
-			if testCase.packetNumber != nil {
-				header.SetPacketNumber(testCase.packetNumber)
-			}
-			l := header.Len(testCase.special)
+				header := packet.Header(buffer[:len(testCase.bytes)])
+				header.SetFlags(testCase.flags)
+				header.SetConnectionID(testCase.connectionID)
+				if testCase.version != nil {
+					header.SetVersion(testCase.version.(uint32))
+				}
+				if testCase.packetNumber != nil {
+					header.SetPacketNumber(testCase.packetNumber)
+				}
+				l := header.Len(testCase.special)
 
-			assert.Equal(t, len(testCase.expectedBytes), l)
-			assert.Equal(t, testCase.expectedBytes, buffer[:l])
-		})
-	}
+				assert.Equal(t, len(testCase.bytes), l)
+				assert.Equal(t, testCase.bytes, buffer[:l])
+			})
+		}
+	})
+
+	t.Run("Read", func(t *testing.T) {
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				header := packet.Header(testCase.bytes)
+				assert.Equal(t, testCase.connectionID, header.ConnectionID())
+				if testCase.version != nil {
+					assert.Equal(t, testCase.version, header.Version())
+				}
+				if testCase.packetNumber != nil {
+					assert.Equal(t, testCase.packetNumber, header.PacketNumber())
+				}
+			})
+		}
+	})
 }
