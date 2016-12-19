88cc9009b3959efa6704632e323d1f45132c5db2
Philipp Brüll <pb@simia.tech>
2016-12-19 12:25:13 +0100

    added type frame

---
+++ frame/type.go
@@ -0,0 +1,109 @@
+package frame
+
+import "fmt"
+
+// Definition of frame types, flags and masks.
+const (
+	TypeStream          = 0x80
+	TypeAcknowledge     = 0x40
+	TypePadding         = 0x00
+	TypeResetStream     = 0x01
+	TypeConnectionClose = 0x02
+	TypeGoAway          = 0x03
+	TypeWindowUpdate    = 0x04
+	TypeBlocked         = 0x05
+	TypeStopWaiting     = 0x06
+	TypePing            = 0x07
+
+	// Flags for the stream type.
+	FlagFinish       = 0x40
+	FlagDataLength   = 0x20
+	FlagOffsetLen8   = 0x07 << 2
+	FlagOffsetLen7   = 0x06 << 2
+	FlagOffsetLen6   = 0x05 << 2
+	FlagOffsetLen5   = 0x04 << 2
+	FlagOffsetLen4   = 0x03 << 2
+	FlagOffsetLen3   = 0x02 << 2
+	FlagOffsetLen2   = 0x01 << 2
+	FlagOffsetLen0   = 0x00 << 2
+	FlagStreamIDLen4 = 0x03
+	FlagStreamIDLen3 = 0x02
+	FlagStreamIDLen2 = 0x01
+	FlagStreamIDLen1 = 0x00
+
+	// Masks for the stream type.
+	MaskStream      = 0x7f
+	MaskOffsetLen   = 0x07 << 2
+	MaskStreamIDLen = 0x03
+
+	// Flags for the acknowledge type.
+	FlagMultiple         = 0x20
+	FlagLargestAckedLen6 = 0x03 << 2
+	FlagLargestAckedLen4 = 0x02 << 2
+	FlagLargestAckedLen2 = 0x01 << 2
+	FlagLargestAckedLen1 = 0x00 << 2
+	FlagAckBlockLen6     = 0x03
+	FlagAckBlockLen4     = 0x02
+	FlagAckBlockLen2     = 0x01
+	FlagAckBlockLen1     = 0x00
+
+	// Masks for the acknowledge type.
+	MaskAcknowledge     = 0x3f
+	MaskLargestAckedLen = 0x03 << 2
+	MaskAckBlockLen     = 0x03
+)
+
+// Type defines the frame type.
+type Type []byte
+
+// SetType sets the frame type.
+func (t Type) SetType(value uint8) {
+	t.ensureLen(1)
+	t[0] = value
+}
+
+// Type returns the frame type.
+func (t Type) Type() uint8 {
+	t.ensureLen(1)
+	if t[0]&TypeStream != 0 {
+		return TypeStream
+	}
+	if t[0]&TypeAcknowledge != 0 {
+		return TypeAcknowledge
+	}
+	return t[0]
+}
+
+// SetFlags sets the flags for the stream and acknowledge type.
+func (t Type) SetFlags(value uint8) {
+	ft := t.Type()
+	if ft == TypeStream {
+		t[0] |= value & MaskStream
+	}
+	if ft == TypeAcknowledge {
+		t[0] |= value & MaskAcknowledge
+	}
+}
+
+// Flags returns the flags for the stream and acknowledge type.
+func (t Type) Flags() uint8 {
+	ft := t.Type()
+	if ft == TypeStream {
+		return t[0] & MaskStream
+	}
+	if ft == TypeAcknowledge {
+		return t[0] & MaskAcknowledge
+	}
+	return 0
+}
+
+// Len returns the length of the frame type.
+func (t Type) Len() int {
+	return 1
+}
+
+func (t Type) ensureLen(l int) {
+	if len(t) < l {
+		panic(fmt.Sprintf("expected buffer to have at least %d bytes, got %d", l, len(t)))
+	}
+}
---
+++ frame/type_test.go
@@ -0,0 +1,68 @@
+package frame_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/simia-tech/go-quic/frame"
+)
+
+func TestHeader(t *testing.T) {
+	testCases := []struct {
+		name string
+
+		frameType uint8
+		flags     uint8
+
+		bytes []byte
+	}{
+		{"StreamCreate", frame.TypeStream, frame.FlagDataLength | frame.FlagOffsetLen8 | frame.FlagStreamIDLen4,
+			[]byte{0xbf}},
+		{"StreamDelete", frame.TypeStream, frame.FlagFinish,
+			[]byte{0xc0}},
+		{"Acknowledge", frame.TypeAcknowledge, frame.FlagMultiple | frame.FlagLargestAckedLen6 | frame.FlagAckBlockLen6,
+			[]byte{0x6f}},
+		{"Padding", frame.TypePadding, 0,
+			[]byte{0x00}},
+		{"ResetStream", frame.TypeResetStream, 0,
+			[]byte{0x01}},
+		{"ConnectionClose", frame.TypeConnectionClose, 0,
+			[]byte{0x02}},
+		{"GoAway", frame.TypeGoAway, 0,
+			[]byte{0x03}},
+		{"WindowUpdate", frame.TypeWindowUpdate, 0,
+			[]byte{0x04}},
+		{"Blocked", frame.TypeBlocked, 0,
+			[]byte{0x05}},
+		{"StopWaiting", frame.TypeStopWaiting, 0,
+			[]byte{0x06}},
+		{"Ping", frame.TypePing, 0,
+			[]byte{0x07}},
+	}
+
+	t.Run("Write", func(t *testing.T) {
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				buffer := make([]byte, len(testCase.bytes))
+
+				ft := frame.Type(buffer)
+				ft.SetType(testCase.frameType)
+				ft.SetFlags(testCase.flags)
+
+				assert.Equal(t, len(testCase.bytes), ft.Len())
+				assert.Equal(t, testCase.bytes, buffer)
+			})
+		}
+	})
+
+	t.Run("Read", func(t *testing.T) {
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				ft := frame.Type(testCase.bytes)
+				assert.Equal(t, testCase.frameType, ft.Type())
+				assert.Equal(t, testCase.flags, ft.Flags())
+			})
+		}
+	})
+}
